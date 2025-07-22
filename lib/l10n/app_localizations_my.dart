5b9761b82bfded21656eb9d18c926e31e8006b8c
KSTA <>
2025-07-22 11:37:32 +0630

    Add localization support for English and Burmese, including ARB files and localization classes.

---
+++ lib/l10n/app_localizations_my.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Burmese (`my`).
+class AppLocalizationsMy extends AppLocalizations {
+  AppLocalizationsMy([String locale = 'my']) : super(locale);
+
+  @override
+  String get helloWorld => 'မင်္ဂလာပါ ကမ္ဘာကြီးရေ';
+
+  @override
+  String get reload => 'ပြန်လုပ်ပါ';
+
+  @override
+  String get nextPage => 'နောက်သို့';
+
+  @override
+  String get nextPageTitle => 'စာမျက်နှာ ၂';
+}
